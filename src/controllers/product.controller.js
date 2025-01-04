812bd302e789bf6621dd9e922bb4a291460483d6
pardhusnc2004 <pardupolisetty2018@gmail.com>
2025-01-04 08:32:02 +0530

    add logic EditProduct and DeleteProduct @product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -26,7 +26,15 @@ export const AddProduct = async (req, res) => {
 
 export const EditProduct = async (req, res) => {
     try {
-        
+        const { user } = req;
+        const { productId } = req.params;
+        const { productName, minBuyQuantity, productCategory, productImages, productDescription, productFeatures } = req.body;
+        const existingProduct = await PRODUCTMODEL.findOne({ productOwner: user._id, _id: productId });
+        if(!existingProduct) {
+            return res.status(403).json({message: "You are not authorized to edit this product/ product invalid"})
+        }
+        const updatedProduct = await PRODUCTMODEL.findByIdAndUpdate(productId, {productName: productName, minBuyQuantity: minBuyQuantity, productCategory: productCategory, productImages: productImages, productDescription: productDescription, productFeatures: productFeatures});
+        return res.status(200).json({message: "Product updated successfully"});
     } catch (error) {
         console.log("Error @EditProduct -> prodcut.controller.js")
         return res.status(500).json({message: "Internal server error"})
@@ -35,7 +43,14 @@ export const EditProduct = async (req, res) => {
 
 export const DeleteProdcut = async (req, res) => {
     try {
-        
+        const { user } = req;
+        const { productId } = req.params;
+        const existingProduct = await PRODUCTMODEL.findOne({ productOwner: user._id, _id: productId });
+        if(!existingProduct) {
+            return res.status(403).json({message: "You are not authorized to delete this product/ product invalid"})
+        }
+        const deletedProduct = await PRODUCTMODEL.findByIdAndDelete(productId);
+        return res.status(200).json({message: "Product deleted successfully"});
     } catch (error) {
         console.log("Error @DeleteProdcut -> prodcut.controller.js")
         return res.status(500).json({message: "Internal server error"})
